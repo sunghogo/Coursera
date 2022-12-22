8679f8c95cb402db83088f257c0ceaefc41ecf1d
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-22 03:42:24 -0500

    CS:PP: Week 1: Fixed ThreeSort to properly sort values in ascending order due to variable overwrite

--- Computer Science Programming with a Purpose/Week 1/ThreeSort.java
+++ Computer Science Programming with a Purpose/Week 1/ThreeSort.java
@@ -6,15 +6,16 @@ public class ThreeSort {
         int num3 = Integer.parseInt(args[2]);
         
         // Sorting Algorithm
-        int minimum, mid, maximum;
+        int minimum, mid, maximum, lastsort;
         minimum = Math.min(num1, num2);
         mid = Math.max(num1, num2);
 
         maximum = Math.max(mid, num3);
         mid = Math.min(mid, num3);
 
+        lastsort = Math.max(minimum, mid);
         minimum = Math.min(minimum, mid);
-        mid = Math.max(minimum, mid);
+        mid = lastsort;
 
 
         // Print in ascending order
