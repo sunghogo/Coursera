cd45101aa9494385303ecd231ecb3864ef485599
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-22 03:27:12 -0500

    CS:PP: Week 1: Fixed ThreeSort to properly sort values in ascending order

--- Computer Science Programming with a Purpose/Week 1/ThreeSort.java
+++ Computer Science Programming with a Purpose/Week 1/ThreeSort.java
@@ -5,10 +5,16 @@ public class ThreeSort {
         int num2 = Integer.parseInt(args[1]);
         int num3 = Integer.parseInt(args[2]);
         
-        // Compute max, min, mid values
-        int maximum = Math.max(num3, Math.max(num1, num2));
-        int minimum = Math.min(num3, Math.min(num1, num2));
-        int mid = Math.max(Math.min(num1, num2), Math.min(num2, num3));
+        // Sorting Algorithm
+        int minimum, mid, maximum;
+        minimum = Math.min(num1, num2);
+        mid = Math.max(num1, num2);
+
+        maximum = Math.max(mid, num3);
+        mid = Math.min(mid, num3);
+
+        minimum = Math.min(minimum, mid);
+        mid = Math.max(minimum, maximum);
 
         // Print in ascending order
         System.out.println(minimum);
