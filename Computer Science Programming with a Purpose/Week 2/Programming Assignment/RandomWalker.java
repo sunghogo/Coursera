8640d55ade24895ddf6ded9e4aa620b32e8516a1
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-27 16:01:54 -0500

    CS:PP: Week 2: Programming Assignment Addition

---
+++ Computer Science Programming with a Purpose/Week 2/Programming Assignment/RandomWalker.java
@@ -0,0 +1,33 @@
+public class RandomWalker {
+    public static void main(String[] args) {
+        // Initialize input r
+        int r = Integer.parseInt(args[0]);
+        
+        // Initialize coordinates, steps, and print initial coordinates
+        int x = 0;
+        int y = 0;
+        int steps = 0;
+        System.out.println("(" + x + ", " + y + ")");
+
+        // Compute random walk until manhattan distance: |x| + |y| = r
+        while ( Math.abs(x) + Math.abs(y) != r) {
+            // Generate random probability 0 - 100
+            int prob = (int) Math.round(Math.random() * 100);
+            if (prob < 25) {
+                x++; 
+            } else if (prob < 50) {
+                y++; 
+            } else if (prob < 75) {
+                x--; 
+            } else {
+                y--; 
+            }
+
+            // Print coordinates at every step and increment step count
+            System.out.println("(" + x + ", " + y + ")");
+            steps++;
+        }
+
+        System.out.println("steps = " + steps);
+    }
+}
