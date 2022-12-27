e223e82d8ff6fd87a9f1b317ade313ef6960a155
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-27 16:41:53 -0500

    CS:PP: Week 2: Programming Assignment Addition

---
+++ Computer Science Programming with a Purpose/Week 2/Programming Assignment/RandomWalkers.java
@@ -0,0 +1,42 @@
+public class RandomWalkers {
+    public static void main(String[] args) {
+        // Initialize inputs r and trials
+        int r = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);         
+        
+        // Intialize running sum of all trial steps 
+        double stepsaverage = 0.0;
+
+        // Iterate numebr of trials
+        for (int i = 0; i < trials; i++) {
+            // Initialize coordinates, steps, and print initial coordinates
+            int x = 0;
+            int y = 0;
+            int steps = 0;
+
+            // Compute random walk until manhattan distance: |x| + |y| = r
+            while ( Math.abs(x) + Math.abs(y) != r) {
+                // Generate random probability 0 - 100
+                int prob = (int) Math.round(Math.random() * 100);
+                if (prob < 25) {
+                    x++; 
+                } else if (prob < 50) {
+                    y++; 
+                } else if (prob < 75) {
+                    x--; 
+                } else {
+                    y--; 
+                }
+                // Increment step count
+                steps++;
+            }
+            // Add total number of steps to running sum
+            stepsaverage += steps;
+        }
+
+        // Calculate average and print
+        stepsaverage /= trials;
+
+        System.out.println("average number of steps = " + stepsaverage);
+    }
+}
