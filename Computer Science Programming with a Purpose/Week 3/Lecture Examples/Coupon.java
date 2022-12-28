443d5f94b938b4d85a59215ee95bdcbc42dda63c
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-27 23:06:47 -0500

    CS:PP: Week 3: Lecture Examples Additions

---
+++ Computer Science Programming with a Purpose/Week 3/Lecture Examples/Coupon.java
@@ -0,0 +1,24 @@
+// Coupon Collector Problem
+// M ln M + 0.57721M
+public class Coupon 
+{
+    public static void main(String[] args)
+    {
+        int M =  Integer.parseInt(args[0]);
+        int cards = 0;      // number of cards collected
+        int distinct = 0;   // number of distinct cards
+
+        boolean[] found = new boolean[M];
+        while (distinct < M)
+        {
+            int r = (int) (Math.random() * M);
+            cards++;
+            if (!found[r])
+            {
+                distinct++;
+                found[r] = true;
+            }
+        }
+        System.out.println(cards);
+    }    
+}
