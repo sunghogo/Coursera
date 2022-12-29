c055e9e11bfc94331cb6e6c594e7397cc35f9f77
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-28 23:46:34 -0500

    CS:PP: Week 3: Programming Assignment: Discrete Distribution: Added Comments

--- Computer Science Programming with a Purpose/Week 3/Programming Assignment/DiscreteDistribution.java
+++ Computer Science Programming with a Purpose/Week 3/Programming Assignment/DiscreteDistribution.java
@@ -1,12 +1,14 @@
 public class DiscreteDistribution {
     public static void main(String[] args)
     {
+        // Initialize M trials and running sum of a1, ..., an
         int M = Integer.parseInt(args[0]);
         int runningSum = 0;
         for (int i = 1; i < args.length; i++) {
             runningSum += Integer.parseInt(args[i]);
         }
 
+        // For M trials, randomly generate number within range of Sn - 1 and find corresponding index/n
         for (int i = 1; i < M+1; i++) {
             double prob = Math.round(Math.random() * (runningSum - 1));
             int index = -1;
@@ -18,6 +20,7 @@ public class DiscreteDistribution {
                 }
             }
 
+            // Handle new lines and white spaces
             System.out.print(index);
             if (i % 25 == 0) {
                 System.out.println();
