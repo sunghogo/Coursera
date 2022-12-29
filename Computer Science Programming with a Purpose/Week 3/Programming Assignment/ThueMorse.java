3ed78ce6482762eb109c5711bd620c947d88ebc8
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-29 11:08:37 -0500

    CS:PP: Week 3: Programming Assignment: Thue-Morse weave

---
+++ Computer Science Programming with a Purpose/Week 3/Programming Assignment/ThueMorse.java
@@ -0,0 +1,43 @@
+public class ThueMorse {
+    public static void main(String[] args) {
+        // Initialize N size 2D array
+        int N = Integer.parseInt(args[0]);
+        String[][] arr = new String[N][N];
+
+        // Generate Thue-Morse Sequence
+        int[] TM = new int[N];
+        int twopower = 2;
+        for (int i = 0; i < N; i++) {
+            if (i == 0 ) {
+                TM[0] = 0;
+            } else if (i == 1) {
+                TM[1] = 1;
+            } else if (i % twopower == 0) {
+                for (int j = i; j < N; j++) {
+                    TM[j] = TM[j-twopower] ^ 1;
+                }
+                twopower = twopower << 1;
+            }
+        }
+
+        // Initialize + and - in arr and print
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (TM[i] == TM[j]) {
+                    arr[i][j] = "+";
+                    System.out.print(arr[i][j]);
+                } else {
+                    arr[i][j] = "-";
+                    System.out.print(arr[i][j]);
+                }
+
+                // Handle whitespaces and new lines
+                if (j + 1 == N) {
+                    System.out.println();
+                } else {
+                    System.out.print(" ");
+                }
+            }
+        }
+    }    
+}
