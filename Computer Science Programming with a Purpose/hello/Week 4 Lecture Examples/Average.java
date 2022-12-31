bc0cc3a5ca1432e614c73b815f61c7b2a978e5a7
sunghogo <70732249+sunghogo@users.noreply.github.com>
2022-12-30 23:47:47 -0500

    CS:PP: Week 4: Lecture Examples Addition

---
+++ Computer Science Programming with a Purpose/hello/Week 4 Lecture Examples/Average.java
@@ -0,0 +1,13 @@
+// CTRL+D or CTRL+Z on Windows to end input stream
+public class Average {
+    public static void main(String[] args) {
+        double sum = 0.0;   // cumulative total
+        int n = 0;          // number of values
+        while (!StdIn.isEmpty()) {
+            double x = StdIn.readDouble();
+            sum = sum + x;
+            n++;
+        }
+        StdOut.println(sum / n);
+    }
+}
