3cc797ac848fbd9f3031f74db301463744cf1243
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-01-01 21:25:01 -0500

    CS:PP: Week 4: Programming Assignment: Shannon entropy

---
+++ Computer Science Programming with a Purpose/hello/Week 4 Programming Assignments/ShannonEntropy.java
@@ -0,0 +1,28 @@
+public class ShannonEntropy {
+    public static void main(String[] args) {
+        // Read from 1 to m integers from StdIn
+        int m = Integer.parseInt(args[0]);
+        int counter = 0;
+        double[] p = new double[m + 1];
+
+        // Read integers from StdIn and fill in tracking array
+        while (!StdIn.isEmpty()) {
+            int x = StdIn.readInt();
+            if (x < m + 1) {
+                p[x]++;
+            }
+            // Increment Counter
+            counter++;
+        }
+        // Calculate and print Shannon Entropy
+        double SE = 0.0;
+        for (int i = 0; i < m + 1; i++) {
+            if (p[i] != 0.0) {
+                double pi = p[i] / counter;
+                SE += (-pi * (Math.log(pi) / Math.log(2)));
+            }
+        }
+        StdOut.printf("%.4f", SE);
+        StdOut.println();
+    }
+}
