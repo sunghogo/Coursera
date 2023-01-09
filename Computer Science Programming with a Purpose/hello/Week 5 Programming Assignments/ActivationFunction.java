e49400f973da9486d94e8e34fe13ec0e5e19d7a2
sunghogo <70732249+sunghogo@users.noreply.github.com>
2023-01-08 22:37:48 -0500

    CS:PP: Week 5: Programming Assignment: Activation Functions

--- Computer Science Programming with a Purpose/hello/Week 5 Programming Assignments/ActivationFunction.java
+++ Computer Science Programming with a Purpose/hello/Week 5 Programming Assignments/ActivationFunction.java
@@ -1,2 +1,32 @@
 public class ActivationFunction {
+        // Returns the Heaviside function of x.
+        public static double heaviside(double x) {
+            return 1.0;
+        }
+
+        // Returns the sigmoid function of x.
+        public static double sigmoid(double x) {
+            return 1.0;
+        }
+    
+        // Returns the hyperbolic tangent of x.
+        public static double tanh(double x) {
+            return 1.0;
+        }
+    
+        // Returns the softsign function of x.
+        public static double softsign(double x) {
+            return 1.0;
+        }
+    
+        // Returns the square nonlinearity function of x.
+        public static double sqnl(double x) {
+            return 1.0;
+        }
+    
+        // Takes a double command-line argument x and prints each activation
+        // function, evaluated, in the format (and order) given below.
+        public static void main(String[] args) {
+            
+        }
 }
